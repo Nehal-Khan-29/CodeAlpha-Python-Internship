2c82d3f67986c43481dc4f9b149f3e74b172eb73
Nehal Khan <128951002+Nehal-Khan-29@users.noreply.github.com>
2024-02-01 17:19:04 +0530

    Update TokenBot.py

--- Task-2/TokenBot.py
+++ Task-2/TokenBot.py
@@ -34,13 +34,20 @@ entrychat.config(relief='sunken')
     
 def send_message():
     user_input = entrychat.get("1.0", "end-1c").strip()
-    displaychat.insert(tk.END, "\nUser: " + user_input + "\n")
+    displaychat.insert(tk.END, "\nUser: ", "user")
+    displaychat.insert(tk.END, user_input + "\n")
     tokens = nltk.word_tokenize(user_input)
     tagged_tokens = nltk.pos_tag(tokens)
     for token, pos_tag in tagged_tokens:
-        displaychat.insert(tk.END, f"Token: {token}, POS: {pos_tag}\n")
+        displaychat.insert(tk.END, f"Token: ", "token")
+        displaychat.insert(tk.END, f"{token}", "normal")
+        displaychat.insert(tk.END, f", POS: {pos_tag}\n")
     entrychat.delete("1.0", tk.END)
-    
+
+displaychat.tag_config("user", foreground="green")
+displaychat.tag_config("token", foreground="blue")
+displaychat.tag_config("normal", foreground="white")
+
 bg_button = Button(root, text="Ask", command=send_message, background='green', foreground='white',font=("Cascadia Mono", 10))
 bg_button.place(x=560,y=560)
 
