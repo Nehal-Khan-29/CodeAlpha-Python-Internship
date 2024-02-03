80677e441404f5951ad51b734d22eda031279d3a
Nehal Khan <128951002+Nehal-Khan-29@users.noreply.github.com>
2024-02-03 16:52:16 +0530

    Update HangMan.py

--- Task-1/HangMan.py
+++ Task-1/HangMan.py
@@ -2,12 +2,15 @@
 
 from random_word import RandomWords
 import os
+import keyboard
 
 # Variables =================================================================================================================
 
 error_limit = 4
 error_count = 0
 random_word = RandomWords().get_random_word()
+wrong=[]
+leng= len(random_word)
 answer=[]
 string = ""
 
@@ -52,6 +55,15 @@ x4="""
         / \  |
              |
         _____|    """
+x5="""
+         _____
+             |
+             |
+             |
+    \O/      |
+     |       |
+    / \ _____| 
+          """
 
 # Game =================================================================================================================
 
@@ -63,6 +75,8 @@ while error_count < error_limit:
     os.system('cls')
     print("\nThe Error limit: 4")
     print("Your Error count: ", error_count)
+    print("Word Length: ", leng)
+    print("Your Error Letters: ", wrong)
     #print(random_word)
     print(x0)
     print("\n",string)
@@ -79,6 +93,7 @@ while error_count < error_limit:
         
     else:
         error_count+=1
+        wrong.append(enter_letter)
         if error_count == 1:
             x0=x1
         elif error_count == 2:
@@ -88,6 +103,7 @@ while error_count < error_limit:
         elif error_count == 4:
             x0=x4
             os.system('cls')
+            print("Your Error Letters: ", wrong)
             print('\033[31m' + x0 + '\033[0m')
             print("\n",string)
             print("\n",'\033[34m' + random_word + '\033[0m')
@@ -97,7 +113,9 @@ while error_count < error_limit:
         os.system('cls')
         print("\nThe Error limit: 4")
         print("Your Error count: ", error_count)
-        print('\033[32m' + x0+'\033[0m')
+        print("Word Length: ", leng)
+        print("Your Error Letters: ", wrong)
+        print('\033[32m' + x5+'\033[0m')
         print("\n",'\033[34m' + string+'\033[0m')
         print('\033[32m'+"\nYou win !!! You saved Him\n"+ '\033[0m')
         break
